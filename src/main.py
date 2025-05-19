24b160815358959e78b078d4c0626c23c513523f
MarcBM <marc.betbeder@hotmail.com>
2025-05-20 00:50:36 +1000

    2.6

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
 from textnode import TextNode, TextType
+from leafnode import LeafNode
 
 def main():
   text_node = TextNode("This is some anchor text", TextType.LINK, "https://example.com")
   print(text_node)
+  
+def text_node_to_html_node(text_node):
+  match text_node.text_type:
+    case TextType.TEXT:
+      return LeafNode(tag=None, value=text_node.text)
+    case TextType.BOLD:
+      return LeafNode(tag="b", value=text_node.text)
+    case TextType.ITALIC:
+      return LeafNode(tag="i", value=text_node.text)
+    case TextType.CODE:
+      return LeafNode(tag="code", value=text_node.text)
+    case TextType.LINK:
+      return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    case TextType.IMAGE:
+      return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+    case _:
+      raise ValueError(f"Unknown text type: {text_node.text_type}")
   
 if __name__ == "__main__":
   main()
